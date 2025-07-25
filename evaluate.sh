3dadf05becce246b5442c79c5c5d79bb07dd5c73
HangeolChang <hangeol@kaist.ac.kr>
2025-07-25 19:52:10 +0900

    bug fix,restore default trl version.

--- evaluate.sh
+++ evaluate.sh
@@ -1,4 +1,27 @@
-CHECKPOINT_ROOT="run/test2"
+#!/bin/bash
+#SBATCH --job-name=unir_eval # create a short name for your job
+#SBATCH --nodes=1
+#SBATCH --nodelist=hpc-pr-a-pod20 # node count
+#SBATCH --ntasks-per-node=1     # total number of tasks per node
+#SBATCH --cpus-per-task=8        # cpu-cores per task (>1 if multi-threaded tasks)
+# #SBATCH --gres=gpu:1    # number of gpus per node
+#SBATCH --time=480:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --output=unir_eval.out
+#SBATCH --error=unir_eval.err
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=hangeol@kaist.ac.kr
+
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export WORLD_SIZE=$(($SLURM_NNODES * SLURM_NTASKS_PER_NODE))
+echo "WORLD_SIZE="$WORLD_SIZE
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
+
+
+CHECKPOINT_ROOT="run/UNIR_MATH_llama"
 CONFIG="recipes/unir.yaml"
 SCRIPT="src/unir/evaluate.py"
 mkdir -p "$CHECKPOINT_ROOT/output_log"
@@ -14,13 +37,13 @@ PROMPT2="A conversation between User and Assistant. The user asks a question, an
 PROMPT3="A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively. Your response should be in the following format: <think>\nYour reasoning here\n</think>\n<answer>\n answer here \n</answer>. The reasoning process Note that respond by English, NOT use other languages."
 
 
-MODEL=qwen # qwen or llama
+MODEL=llama # qwen or llama
 DATA=MATH # MATH or GSM8K
 MODEL_SIZE=3b            # 3b, 7b, 8b 또는 14b
 
 
 
-
+export CUDA_VISIBLE_DEVICES=6
 
 case $MODEL in
   qwen)
@@ -97,30 +120,31 @@ case $DATA in
     ;;
 esac
 
+PROMPT_VAR="PROMPT${PROMPT_NO}"
+SYSTEM_PROMPT="${!PROMPT_VAR}"
 
-
-for ckpt_num in $(seq 10 10 20); do
-  for dataset_index in 0 1 2 3; do
-      LOG_PATH="${CHECKPOINT_ROOT}/output_log/checkpoint-${ckpt_num}_dataset_${dataset_index}"
-      python "$SCRIPT" \
-      --config "$CONFIG" \
-      --dataset_config default \
-      --num_generations 1 \
-      --per_device_eval_batch_size $EVAL_SIZE \
-      --max_completion_length $MAX_LENGTH \
-      --gradient_checkpointing false \
-      --model_name_or_path "$MAIN_MODEL" \
-      --ref_name_or_path "$REF_MODEL" \
-      --eval_checkpoint "$CHECKPOINT_ROOT"/checkpoint-$ckpt_num \
-      --use_vllm false \
-      --output_dir "$LOG_PATH" \
-      --run_name unir_test \
-      --reward_funcs "$REWARD" \
-      --reward_weights 1.0 \
-      --temperature 0.0 \
-      --beta 0.0 \
-      --dataset_index $dataset_index \
-      --system_prompt "$SYSTEM_PROMPT" \
-      > "${LOG_PATH}.log" 2>&1
-    done
+for dataset_index in 0 1 2 3; do
+  for ckpt_num in $(seq 10 10 20); do
+      LOG_PATH="${CHECKPOINT_ROOT}/output_log/checkpoint-${ckpt_num}_dataset_${dataset_index}_seed_${SEED}_temp0_3"
+    python "$SCRIPT" \
+    --config "$CONFIG" \
+    --dataset_config default \
+    --num_generations 1 \
+    --per_device_eval_batch_size $EVAL_SIZE \
+    --max_completion_length $MAX_LENGTH \
+    --gradient_checkpointing false \
+    --model_name_or_path "$MAIN_MODEL" \
+    --ref_name_or_path "$REF_MODEL" \
+    --eval_checkpoint "$CHECKPOINT_ROOT"/checkpoint-$ckpt_num \
+    --use_vllm false \
+    --output_dir "$LOG_PATH" \
+    --run_name unir_test \
+    --reward_funcs "$REWARD" \
+    --reward_weights 1.0 \
+    --temperature 0.0 \
+    --beta 0.0 \
+    --dataset_index $dataset_index \
+    --system_prompt "$SYSTEM_PROMPT" \
+    > "${LOG_PATH}.log" 2>&1
+  done
 done
--- setup.py
+++ setup.py
@@ -55,19 +55,19 @@ _deps = [
     "langdetect",  # Needed for LightEval's extended tasks
     "latex2sympy2_extended>=1.0.6",
     "liger_kernel==0.5.3",
+    "lighteval @ git+https://github.com/huggingface/lighteval.git@ed084813e0bd12d82a06d9f913291fdbee774905",
     "math-verify==0.5.2",  # Used for math verification in grpo
     "packaging>=23.0",
     "parameterized>=0.9.0",
     "peft>=0.14.0",
     "pytest",
-    "pylatexenc",
     "python-dotenv",
     "ruff>=0.9.0",
     "safetensors>=0.3.3",
     "sentencepiece>=0.1.99",
     "torch==2.5.1",
-    "transformers==4.53.1",
-    "trl==0.19.1",
+    "transformers==4.49.0",
+    "trl @ git+https://github.com/huggingface/trl.git@69ad852e5654a77f1695eb4c608906fe0c7e8624",
     "vllm==0.7.2",
     "wandb>=0.19.1",
     "tensorboard",
@@ -89,6 +89,7 @@ def deps_list(*pkgs):
 
 extras = {}
 extras["torch"] = deps_list("torch")
+
 
 # core dependencies shared across the whole project - keep this to a bare minimum :)
 install_requires = [
@@ -113,7 +114,7 @@ install_requires = [
 
 setup(
     name="unir",
-    version="0.2.0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
+    version="0.1.0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     description="UniR",
     keywords="Universal Reasoner",
     license="Apache",
